438642b3c58a995a5fecda55e8c16bc8fe71431a
kashyap <kashyap10115@gmail.com>
2025-08-30 00:41:52 +0530

    version 01

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// script.js
-function showSection(sectionId) {
-  const sections = document.querySelectorAll('.data-section');
-  sections.forEach(sec => sec.classList.add('hidden'));
-  document.getElementById(sectionId).classList.remove('hidden');
+const texts = document.querySelectorAll(".rotating-text p");
+let index = 0;
+
+function showNextText() {
+  texts.forEach((t, i) => {
+    t.classList.remove("active");
+  });
+  texts[index].classList.add("active");
+  index = (index + 1) % texts.length;
 }
+
+showNextText();
+setInterval(showNextText, 3000); // every 3 seconds
